2018b12944fa23641dcddf41696e1e0b9b1c46c9
Sebastian Cole <sebastian.paris@gmail.com>
2014-04-19 23:11:38 +1000

    Extracting deck logic into its own file

---
+++ deck.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Deck struct {
+	numberInDeck   int
+	Active         bool
+	availableCards []Card
+	burntCard      interface{}
+	discardedCards interface{}
+}
+
+func (d Deck) NumberInDeck() int { return d.numberInDeck }
+
+func NewDeck() Deck {
+	princess := Card{8, "Princess", "Discarding this card loses the game"}
+	countess := Card{7, "Countess", "Must discard this card when held with a King or Prince"}
+	king := Card{6, "King", "Trade hands with another player of your choice"}
+	prince := Card{5, "Prince", "Choose any player (including yourself) to discard his or her hand and draw a new card"}
+	handmaiden := Card{4, "Handmaiden", "Until your next turn, ignore all effects from other players cards"}
+	baron := Card{3, "Baron", "You and another player secretly compare hands. The player with the lower value is out of the round"}
+	priest := Card{2, "Priest", "Look at another players hand"}
+	guard := Card{1, "Guard", "Name a non-Guard card and choose another player. If that player has that card, he or she is out of the round"}
+
+	var availableCards []Card
+
+	availableCards = append(availableCards, princess)
+	availableCards = append(availableCards, countess)
+	availableCards = append(availableCards, king)
+	for i := 0; i < 2; i++ {
+		availableCards = append(availableCards, prince)
+	}
+	for i := 0; i < 2; i++ {
+		availableCards = append(availableCards, handmaiden)
+	}
+	for i := 0; i < 2; i++ {
+		availableCards = append(availableCards, baron)
+	}
+	for i := 0; i < 2; i++ {
+		availableCards = append(availableCards, priest)
+	}
+	for i := 0; i < 5; i++ {
+		availableCards = append(availableCards, guard)
+	}
+	deck := Deck{len(availableCards), true, availableCards, nil, nil}
+
+	return deck
+}
+
+func (d Deck) Describe() {
+	if d.Active {
+		fmt.Println("Deck is active")
+	} else {
+		fmt.Println("Deck is not active")
+	}
+	for _, card := range d.availableCards {
+		card.Describe()
+	}
+}
+
+func (d Deck) Draw(numberOfCards int) []Card {
+	var drawnCards []Card
+	for i := 0; i < numberOfCards; i++ {
+		//Random Number
+		// Pop card from availableCards
+		//return cards
+	}
+	return drawnCards
+}
---
+++ deck_test.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestNewDeckIsReadyForPlay(t *testing.T) {
+	deck := NewDeck()
+	if !deck.Active {
+		t.Fail()
+	}
+	return
+}
+
+func TestNumberOfCardsInDeck(t *testing.T) {
+	princess := Card{8, "Princess", "Discarding this card loses the game"}
+	countess := Card{7, "Countess", "Must discard this card when held with a King or Prince"}
+	var availableCards []Card
+
+	availableCards = append(availableCards, princess)
+	availableCards = append(availableCards, countess)
+	deck := Deck{len(availableCards), true, availableCards, nil, nil}
+
+	if !(deck.NumberInDeck() == 2) {
+		t.Error("After initialisation, Deck did not contain 2 cards.")
+	}
+}
+
+func TestDrawingCardsFromDeck(t *testing.T) {
+	princess := Card{8, "Princess", "Discarding this card loses the game"}
+	var availableCards []Card
+	var drawnCards []Card
+	availableCards = append(availableCards, princess)
+	deck := Deck{len(availableCards), true, availableCards, nil, nil}
+
+	if deck.NumberInDeck() != 1 {
+		t.Fatalf("Deck should contain 1 card, but contains %d cards", deck.NumberInDeck())
+	}
+
+	drawnCards = deck.Draw(1)
+	if drawnCards[0].Name() != "Princess" {
+		t.Error("The wrong card was drawn from the deck")
+	}
+
+	if deck.NumberInDeck() != 0 {
+		t.Errorf("Deck should be empty, but contains %d cards", deck.NumberInDeck())
+	}
+}
+
+func ExampleF() {
+	deck := NewDeck()
+	deck.Describe()
+
+	// Output:
+	// Deck is active
+	// Card: Princess is worth 8 points, when played has "Discarding this card loses the game" effect
+	// Card: Countess is worth 7 points, when played has "Must discard this card when held with a King or Prince" effect
+	// Card: King is worth 6 points, when played has "Trade hands with another player of your choice" effect
+	// Card: Prince is worth 5 points, when played has "Choose any player (including yourself) to discard his or her hand and draw a new card" effect
+	// Card: Prince is worth 5 points, when played has "Choose any player (including yourself) to discard his or her hand and draw a new card" effect
+	// Card: Handmaiden is worth 4 points, when played has "Until your next turn, ignore all effects from other players cards" effect
+	// Card: Handmaiden is worth 4 points, when played has "Until your next turn, ignore all effects from other players cards" effect
+	// Card: Baron is worth 3 points, when played has "You and another player secretly compare hands. The player with the lower value is out of the round" effect
+	// Card: Baron is worth 3 points, when played has "You and another player secretly compare hands. The player with the lower value is out of the round" effect
+	// Card: Priest is worth 2 points, when played has "Look at another players hand" effect
+	// Card: Priest is worth 2 points, when played has "Look at another players hand" effect
+	// Card: Guard is worth 1 points, when played has "Name a non-Guard card and choose another player. If that player has that card, he or she is out of the round" effect
+	// Card: Guard is worth 1 points, when played has "Name a non-Guard card and choose another player. If that player has that card, he or she is out of the round" effect
+	// Card: Guard is worth 1 points, when played has "Name a non-Guard card and choose another player. If that player has that card, he or she is out of the round" effect
+	// Card: Guard is worth 1 points, when played has "Name a non-Guard card and choose another player. If that player has that card, he or she is out of the round" effect
+	// Card: Guard is worth 1 points, when played has "Name a non-Guard card and choose another player. If that player has that card, he or she is out of the round" effect
+
+}
